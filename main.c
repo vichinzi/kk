03e5ff8a6d5ebc2d2d823cfdb875ef091c1357b2
vichinzi <44653674+vichinzi@users.noreply.github.com>
2018-11-02 23:24:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,21 +7,21 @@
 int main(int argc, char const *argv[]){ 
 
     int T,k;
-    scanf("%d", &T);
+    scanf("%d", &T); // cantidad de consultas
     char *C = (char *)malloc(33*sizeof(char));
     char *M;
 
     for(int i = 0; i < T ; i++){
-        scanf("%s", C);
-        scanf("%d",&k);
+        scanf("%s", C);    //String a desencriptar
+        scanf("%d",&k);    //LLave, que contiene la permutacion que se busca
 
-        if(strlen(C)>32){
-            printf("Error: String ingresado no debe tener mas de 32 caracteres\n");
+        if(strlen(C)>20){
+            printf("Error: String ingresado no debe tener mas de 20 caracteres\n");
             continue;
         }
 
         M = F(C,k);
-        printf("%s\n",M );
+        printf("%s\n",M );  //Se imprime string desencriptado
         fflush(stdin);
         free ((void *)M);
     }
